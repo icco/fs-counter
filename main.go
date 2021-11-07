5163ce715143b56313e9f88a127fa6e5c573a390
Nat Welch <nat@natwelch.com>
2021-11-07 18:55:24 +0000

    first pass

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -32,11 +36,39 @@ func main() {
 	viper.AddConfigPath(".")
 
 	if err := viper.ReadInConfig(); err != nil {
-		panic(fmt.Errorf("Fatal error config file: %w", err))
+		log.Fatalf("Fatal error config file: %+v", err)
 	}
 
-	var c Config
+	var c *Config
 	if err := viper.Unmarshal(&c); err != nil {
-		panic(fmt.Errorf("Fatal error unmarshaling: %w", err))
+		log.Fatalf("Fatal error unmarshaling: %+v", err)
 	}
+
+	log.Printf("found config: %+v", c)
+
+	counts := map[string]int64{}
+
+	for _, pc := range c.Paths {
+		log.Printf("searching path: %+v", pc)
+		fileSystem := os.DirFS(pc.FilePath)
+		if err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return fmt.Errorf("could not walk %q: %w", path, err)
+			}
+
+			// Ignore hidden files
+			if strings.HasPrefix(path, ".") {
+				return nil
+			}
+
+			counts[pc.FilePath] += 1
+
+			fmt.Println(path)
+			return nil
+		}); err != nil {
+			log.Fatalf("walk error: %+v", err)
+		}
+	}
+
+	log.Printf("Found: %+v", counts)
 }
